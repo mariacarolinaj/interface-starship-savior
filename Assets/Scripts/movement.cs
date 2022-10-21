7a115e4bbbdc33b7412d9f19010778fbe31bb964
MrMaguila <gabrielgf6000@gmail.com>
2022-10-20 23:40:45 -0300

    limited movement to screen and added new asteroids

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -20,16 +20,36 @@ public class movement : MonoBehaviour
 
         //Debug.Log(x);
 
-        if (x != 0)// && rb.velocity.x < 60) 
+        if (x != 0)
         {
             rb.velocity = rb.velocity + new Vector2(x * 4, 0);
             //transform.position = transform.position + new Vector3(0, 100, 0);
         }
 
-        if(y != 0)// && rb.velocity.y < 60)
+        if(y != 0)
         {
             rb.velocity = rb.velocity + new Vector2(0, y * 4);
             //transform.position = transform.position + new Vector3(100, 0, 0);
         }
+	
+	Camera camera = Camera.main;
+	Vector2 relPos = new Vector2(rb.position.x - camera.transform.position.x, 
+		rb.position.y - camera.transform.position.y);
+	
+	float x_distance_to_border = relPos.x;
+
+	float y_distance_to_border = relPos.y;
+
+	if (Mathf.Abs(x_distance_to_border) + 37.5f > camera.orthographicSize * camera.aspect) {
+		rb.velocity = new Vector2(0, rb.velocity.y);
+		rb.position = new Vector2((Mathf.Abs(x_distance_to_border) / 
+				x_distance_to_border) * (camera.orthographicSize * camera.aspect - 37.6f), rb.position.y);
+	}
+	
+	if (Mathf.Abs(y_distance_to_border) + 40.0f > camera.orthographicSize) {
+		rb.velocity = new Vector2(rb.velocity.x, 0);
+		rb.position = new Vector2(rb.position.x, (Mathf.Abs(y_distance_to_border) / 
+				y_distance_to_border) * (camera.orthographicSize - 40.1f));
+	}
     }
 }
